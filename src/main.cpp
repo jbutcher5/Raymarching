1f8fd2699d154d4ead56abd6c1b023e2b2451a28
James Butcher <jamesbutcher167@gmail.com>
2021-08-31 23:27:09 +0100

    Add basic sphere tracing

--- src/main.cpp
+++ src/main.cpp
@@ -29,17 +29,24 @@ int main(){
   point.set(0., 1.);
 
   Circle x;
-  x.radius = 50;
-  x.pos.set(5., 0.);
+  x.radius = 25;
+  x.pos.set(100., 20.);
 
   InitWindow(screenWidth, screenHeight, screenTitle);
 
   SetTargetFPS(60);
 
   while (!WindowShouldClose()){
+    if (IsKeyDown(KEY_RIGHT)) point.x += 2.0f;
+    if (IsKeyDown(KEY_LEFT)) point.x -= 2.0f;
+    if (IsKeyDown(KEY_UP)) point.y -= 2.0f;
+    if (IsKeyDown(KEY_DOWN)) point.y += 2.0f;
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawCircle(WtoS(point.x, true), WtoS(point.y), 2, RED);
+    DrawCircleV(WtoS(point.expose()), 2, RED);
+    DrawCircleV(WtoS(x.pos.expose()), x.radius, GREEN);
+    DrawCircleV(WtoS(point.expose()), x.signedDistance(point), (Color){20, 20, 200, 50});
     EndDrawing();
   }
 
