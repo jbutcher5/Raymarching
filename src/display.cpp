685a45dec4c3bfacf349923ac75a838ded9eb182
James Butcher <jamesbutcher167@gmail.com>
2021-09-01 21:10:23 +0100

    Expose raw Vector2

--- src/display.cpp
+++ src/display.cpp
@@ -1,8 +1,7 @@
 #pragma once
 #include "display.hpp"
-#include "circle.hpp""
+#include "circle.hpp"
 #include <raylib.h>
-
 
 
 Display::Display(
@@ -11,29 +10,30 @@ Display::Display(
     const char * title,
     int targetFPS
 ){
-    InitWindow(screenWidth, screenHeight, title);
-    SetTargetFPS(targetFPS);
+  InitWindow(screenWidth, screenHeight, title);
+  SetTargetFPS(targetFPS);
+
+  this->screenWidth = screenWidth;
+  this->screenHeight = screenHeight;
 }
 
 Vector2 Display::WtoS(Vector2 position){
   // Convert world space to screen space
   return (Vector2){
-    position.x+(screenWidth/2),
-    position.y+(screenHeight/2)
+    position.x+(this->screenWidth/2),
+    position.y+(this->screenHeight/2)
   };
 }
 
 void Display::attachShape(Circle* shape){
-    shapes.push_back(shape);
+  shapes.push_back(shape);
 }
 
 void Display::drawShape(){
-    BeginDrawing();
-    for (auto i : shapes)
-        DrawCircleV(WtoS(i->pos), i->radius, GREEN);
-    EndDrawing();
+  for (auto i : shapes)
+    DrawCircleV(WtoS(i->pos.expose()), i->radius, GREEN);
 }
 
 void Display::closeScreen(){
-    CloseWindow();
+  CloseWindow();
 }
