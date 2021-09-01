4075f5f167ceaa0f64f474e83b0bdc4acc5107be
James Butcher <jamesbutcher167@gmail.com>
2021-09-01 21:08:32 +0100

    Add constructor

--- src/circle.hpp
+++ src/circle.hpp
@@ -7,6 +7,11 @@ struct Circle{
   Vec2 pos;
   float radius;
 
+  Circle(float rad, float x, float y){
+    this->radius = rad;
+    this->pos.set(x, y);
+  }
+
   float signedDistance(const Vec2& point) const{
     return sqrt(
       power2(pos.x-point.x)+power2(pos.y-point.y)
