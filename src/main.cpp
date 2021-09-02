771d9d494fab4136383b676b98614fa6bdaef084
James Butcher <jamesbutcher167@gmail.com>
2021-09-03 00:51:22 +0100

    Implement sphere tracing

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,54 @@
 #include <cmath>
 #include <vector>
 
-
 #define LOG(x) std::cout << x << std::endl
 #define screenWidth 800
 #define screenHeight 450
 #define screenTitle "[Example] Ray Marching"
 #define screenFPS 60
+#define minRadius 2
+#define depth 200
 
+
+std::vector<Circle> raymarch(
+  Vec2 point,
+  const std::vector<Circle> objects,
+  const Vec2 direction
+){
+  std::vector<Circle> result;
+
+  if (
+    direction.x > 1 ||
+    direction.x < -1 ||
+    direction.y > 1 ||
+    direction.y < -1
+  )
+    return result;
+
+  float distance;
+
+  int i = 0;
+
+  while (i < depth){
+    distance = INFINITY;
+
+    for (auto i : objects)
+      if (i.signedDistance(point) < distance)
+        distance = i.signedDistance(point);
+
+    if (distance < minRadius)
+      return result;
+
+    result.push_back((Circle){distance, point.x, point.y});
+
+    point = ((Vec2){distance, distance}*direction)+point;
+
+    i++;
+  }
+
+  return std::vector<Circle>{};
+
+}
 
 Vector2 WtoS(Vector2 position){
   // Convert world space to screen space
@@ -48,16 +89,14 @@ int main(){
     if (IsKeyDown(KEY_UP)) point.y -= 2.0f;
     if (IsKeyDown(KEY_DOWN)) point.y += 2.0f;
 
-    distance = INFINITY;
-
-    for (auto i : x)
-      if (i.signedDistance(point) < distance)
-        distance = i.signedDistance(point);
+    std::vector<Circle> traces = raymarch(point, x, (Vec2){1,0});
 
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawCircleV(WtoS(point.expose()), 2, RED);
-    DrawCircleV(WtoS(point.expose()), distance, (Color){20, 20, 200, 50});
+    //DrawCircleV(WtoS(point.expose()), distance, (Color){20, 20, 200, 50});
+    for (Circle i : traces)
+      DrawCircleV(WtoS(i.pos.expose()), i.radius, (Color){20, 20, 200, 50});
     screen.drawShape();
     EndDrawing();
 
