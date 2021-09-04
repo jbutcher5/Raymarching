aef665e4b3b7335d8eca8378036593690d425773
James Butcher <jamesbutcher167@gmail.com>
2021-09-04 14:36:40 +0100

    Add squares

--- src/object.hpp
+++ src/object.hpp
@@ -1,11 +1,35 @@
 #pragma once
 #include "vec2.hpp"
 #include <cmath>
+#include <algorithm>
 #include <raylib.h>
+#include <iostream>
 
+#define toVec2(x) (Vec2){x, x}
+
+static Vec2 abs(Vec2 x){
+  return (Vec2){std::abs(x.x), std::abs(x.y)};
+}
+
+static Vec2 max(Vec2 x, Vec2 y){
+  Vec2 result;
+
+  result.x = std::fmaxf(x.x, y.x);
+  result.y = std::fmaxf(x.y, y.y);
+
+  return result;
+}
+
+static Vec2 min(Vec2 x, Vec2 y){
+  Vec2 result;
+
+  result.x = std::fminf(x.x, y.x);
+  result.y = std::fminf(x.y, y.y);
+
+  return result;
+}
 
 struct Object{
-public:
   Vec2 pos;
 
   virtual float signedDistance(const Vec2& point) const = 0;
@@ -13,7 +37,6 @@ public:
 };
 
 struct Circle : public Object{
-  Vec2 pos;
   float radius;
 
   Circle(){
@@ -37,4 +60,40 @@ struct Circle : public Object{
 
     DrawCircleV(screenPosition, this->radius, GREEN);
   }
+};
+
+struct Square : public Object{
+  Vec2 width;
+
+  Square(){
+    Square(0., 0., 0., 0.);
+  }
+
+  Square(float x, float y, float hx, float hy){
+    pos = {x, y};
+    width = {hx, hy};
+  }
+
+  float signedDistance(const Vec2& point) const override{
+    //float d = (Vec2){(float)std::fmax(point.x - width.x, 0.), (float)std::fmax(point.y - width.y, 0.)}.length();
+    //std::cout << point.x - width.x << std::endl;
+
+    Vec2 x = pos - point;
+
+    Vec2 d = abs(x)-width;
+
+    float f = (Vec2){(max(d,(Vec2){0., 0.})) + toVec2(std::fmin(std::max(d.x,d.y), 0.))}.length();
+
+    //std::cout << f << std::endl;
+
+    return f;
+  }
+
+  void draw(const int screenWidth, const int screenHeight) const override{
+    Vec2 screenPosition = {pos.x+(screenWidth/2), pos.y+(screenHeight/2)};
+    screenPosition = screenPosition - width;
+
+    DrawRectangleV(screenPosition.expose(), (width * toVec2(2)).expose(), GREEN);
+  }
+
 };
